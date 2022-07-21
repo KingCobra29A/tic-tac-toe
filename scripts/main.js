3f8c7665e178f5bf17b275c742bd970d05eac250
KingCobra29A <brunerk@umich.edu>
2022-07-21 14:54:44 -0400

    Add initBoard functionality to DisplayController

--- scripts/main.js
+++ scripts/main.js
@@ -19,8 +19,39 @@ const GameBoard = (() => {
 })();
 
 //mode for controlling display to the DOM
+//input parameter is DOM selector where the board will be rendered 
 const DisplayController = (() => {
 
+    let _DOM;
 
-    return {};
+    //each element is _squares is an array: [dom-selector, index]
+    let _squares = [];
+    
+    const _toggleSquare = (index) => {
+        console.log(index);
+    };
+
+    const _renderBoard = () => {
+
+        _DOM.classList.add("game-board");
+
+        for(let i = 0; i < 9; i++){
+            _squares[i] = [document.createElement("div"), i];
+            _squares[i][0].classList.add("square");
+            _DOM.appendChild(_squares[i][0]);
+            _squares[i][0].addEventListener('click', () => _toggleSquare(_squares[i][1]));
+        };
+    };
+
+    const initBoard = (DOM_selector) => {
+        _DOM = DOM_selector;
+        _renderBoard();
+    };
+
+    return {initBoard};
+
 })();
+
+
+
+DisplayController.initBoard(document.querySelector(".game-board-location"));
